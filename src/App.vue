d5051aeb9f32b31bbef8c32c7e621b9840177005
LELI <llii0202@student.monash.edu>
2025-08-27 22:40:09 +1000

    chore: init vue3 project with bootstrap responsiveness (BR A.1 & A.2)

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container py-5">
+    <h1 class="text-center mb-4">Youth Mental Health & Wellbeing</h1>
+    <div class="row">
+      <div class="col-12 col-md-6 mb-3">
+        <div class="card p-3 shadow-sm h-100">
+          <h2 class="h5">About</h2>
+          <p>This is the starter version of our app (Version 1 - Init).</p>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 mb-3">
+        <div class="card p-3 shadow-sm h-100">
+          <h2 class="h5">Responsive Test</h2>
+          <p>Resize the window to see how the layout adapts.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
